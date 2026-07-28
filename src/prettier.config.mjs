16bdde55c3f99a430a41b66257e408e380f1ff05
dSkudilo <dskudilo@gmail.com>
2026-07-28 22:42:53 +0300

    init

---
+++ src/prettier.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  trailingComma: "none",
+  tabWidth: 2,
+  useTabs: true,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParents: "avoid",
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderParserPlugins: [
+    "classProperties",
+    "decorators-legacy",
+    "typescript"
+  ],
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^./(.*)$"
+  ],
+  plugins: ["@trivago/prettier-plugin-sort-imports"]
+}
